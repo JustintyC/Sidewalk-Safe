dc9a1ed932046b8f7e2cf3e3591c07c40738fd79
JustintyC <justin.ty.cheung@gmail.com>
2024-01-20 13:51:07 -0800

    merged test to main

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
 var map = L.map('map').setView([51.505, -0.09], 13);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    attribution: '© OpenStreetMap contributors'
 }).addTo(map);
+
+map.on('click', function(e) {
+    var description = prompt("Enter a description for this place:");
+    if(description) {
+        L.marker(e.latlng).addTo(map)
+            .bindPopup(description)
+            .openPopup();
+    }
+});
+
+var customIcon = L.icon({
+    iconUrl: 'path/to/icon.png',
+    iconSize: [38, 38], // size of the icon
+    iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+    popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+// Use customIcon in L.marker
+L.marker(e.latlng, {icon: customIcon}).addTo(map)
+    .bindPopup(description)
+    .openPopup();
