3347accfac71717c6ef00f28529660e2550470ef
JustintyC <justin.ty.cheung@gmail.com>
2024-01-20 13:55:02 -0800

    changed location to ramen danbo at kerrisdale
    its a good restaurant

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([49.234972, -123.159299], 13);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© OpenStreetMap contributors'
