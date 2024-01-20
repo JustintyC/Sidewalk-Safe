022ccea0aca69600a4b719f62a486ad053478d52
JustintyC <justin.ty.cheung@gmail.com>
2024-01-20 15:39:28 -0800

    button to add pins on map

--- main.js
+++ main.js
@@ -1,26 +1,34 @@
-var map = L.map('map').setView([49.262401839237086, -123.24506350086884], 13);
+var map = L.map('map').setView([49.262401839237086, -123.24506350086884], 15);
+var selectingLocation = false;
+
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
-map.on('click', function(e) {
-    var description = prompt("Enter a description for this place:");
-    if(description) {
-        L.marker(e.latlng).addTo(map)
-            .bindPopup(description)
-            .openPopup();
-    }
-});
-
 var customIcon = L.icon({
-    iconUrl: 'path/to/icon.png',
-    iconSize: [38, 38], // size of the icon
-    iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
-    popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+    iconUrl: 'gregor 50x50.jpg',
+    iconSize: [38, 38],
+    iconAnchor: [19, 38], // point of the icon which will correspond to marker's location
+    popupAnchor: [0, -38] // point from which the popup should open relative to the iconAnchor
 });
 
-// Use customIcon in L.marker
-L.marker(e.latlng, {icon: customIcon}).addTo(map)
-    .bindPopup(description)
-    .openPopup();
+map.on('click', function(e) {
+    if (selectingLocation) {
+        var description = prompt("Enter a description for this place:");
+        if(description) {
+            L.marker(e.latlng, {icon: customIcon}).addTo(map) // Use customIcon here
+                .bindPopup(description)
+                .openPopup();
+        }
+    }
+    selectingLocation = false;    
+    document.getElementById('map').style.cursor = '';
+});
+
+
+function onAddButtonPress() {
+    selectingLocation = true;
+    document.getElementById("map").style.cursor = "crosshair";
+    // TODO prompt user to choose a location
+}
